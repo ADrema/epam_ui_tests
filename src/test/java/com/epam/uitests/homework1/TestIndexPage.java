7e1763fcf29aa68ad5285637defd15c3ba5b1a90
Anastasiia_Dremina <anastasyadremina@rambler.ru>
2018-05-24 10:09:13 +0300

    Homework 1. Some elements were moved to collections

--- src/test/java/com/epam/uitests/homework1/TestIndexPage.java
+++ src/test/java/com/epam/uitests/homework1/TestIndexPage.java
@@ -6,10 +6,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.openqa.selenium.By.id;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class TestIndexPage {
@@ -17,7 +22,7 @@ public class TestIndexPage {
 
 
     @BeforeMethod
-    public void openBrowser() {
+    public void beforeMethod() {
 //        Set options to avoid browser pop-up windows
         ChromeOptions options = new ChromeOptions();
 //        options.addArguments("disable-infobars")
@@ -31,7 +36,7 @@ public class TestIndexPage {
     }
 
     @AfterMethod
-    public void closeBrowser() {
+    public void afterMethod() {
         driver.close();
         driver.quit();
     }
@@ -42,7 +47,7 @@ public class TestIndexPage {
         driver.navigate().to("https://epam.github.io/JDI ");
 
 //        2. Assert Browser title
-        Assert.assertEquals(driver.getTitle(), "Home Page");
+        assertEquals(driver.getTitle(), "Home Page");
 
 //        3. Perform login
         WebElement profile = driver.findElement(By.className("profile-photo"));
@@ -57,10 +62,10 @@ public class TestIndexPage {
         WebElement userName = driver.findElement(By.xpath(".//span[contains(text(), 'Piter Chailovskii')]"));
 
 //        4. Assert User name in the left-top side of screen that user is loggined
-        Assert.assertEquals(userName.getText(), "PITER CHAILOVSKII");
+        assertEquals(userName.getText(), "PITER CHAILOVSKII");
 
 //        5. Assert Browser title
-        Assert.assertEquals(driver.getTitle(), "Home Page");
+        assertEquals(driver.getTitle(), "Home Page");
 
 //        6. Assert that there are 4 items on the header section are displayed and they have proper texts
         WebElement homeItem = driver.findElement(By.xpath(".//a[@href = 'index.html']"));
@@ -68,38 +73,41 @@ public class TestIndexPage {
         WebElement serviceItem = driver.findElement(By.xpath(".//ul[contains(@class, 'uui-navigation')]//a[@class = 'dropdown-toggle']"));
         WebElement metalsAndColorsItem = driver.findElement(By.xpath(".//a[@href = 'metals-colors.html']"));
 
-        Assert.assertEquals(homeItem.getText(), "HOME");
-        Assert.assertEquals(contactFormItem.getText(), "CONTACT FORM");
-        Assert.assertEquals(serviceItem.getText(), "SERVICE");
-        Assert.assertEquals(metalsAndColorsItem.getText(), "METALS & COLORS");
+        assertEquals(homeItem.getText(), "HOME");
+        assertEquals(contactFormItem.getText(), "CONTACT FORM");
+        assertEquals(serviceItem.getText(), "SERVICE");
+        assertEquals(metalsAndColorsItem.getText(), "METALS & COLORS");
 
 //        7. Assert that there are 4 images on the Index Page and they are displayed
-        WebElement practiseIcon = driver.findElement(By.xpath(".//div[@class = 'row clerafix benefits']//span[contains(@class, 'icon-practise')]"));
-        WebElement customIcon = driver.findElement(By.xpath(".//div[@class = 'row clerafix benefits']//span[contains(@class, 'icon-custom')]"));
-        WebElement multiIcon = driver.findElement(By.xpath(".//div[@class = 'row clerafix benefits']//span[contains(@class, 'icon-multi')]"));
-        WebElement baseIcon = driver.findElement(By.xpath(".//div[@class = 'row clerafix benefits']//span[contains(@class, 'icon-base')]"));
 
-        practiseIcon.isDisplayed();
-        customIcon.isDisplayed();
-        multiIcon.isDisplayed();
-        baseIcon.isDisplayed();
+        List<WebElement> imageElements = driver.findElements(By.xpath(".//div[@class = 'row clerafix benefits']//div[contains(@class, 'benefit-icon')]"));
+
+        imageElements.get(0).isDisplayed();
+        imageElements.get(1).isDisplayed();
+        imageElements.get(2).isDisplayed();
+        imageElements.get(3).isDisplayed();
 
 //        8. Assert that there are 4 texts on the Index Page under icons and they have proper text
-        WebElement textUderPractiseIcon = driver.findElement(By.xpath(".//div[@class = 'row clerafix benefits']/div[position()=1]//span[@class = 'benefit-txt']"));
-        WebElement textUnderCustomItem = driver.findElement(By.xpath(".//div[@class = 'row clerafix benefits']/div[position()=2]//span[@class = 'benefit-txt']"));
-        WebElement textUnderMultiIcon = driver.findElement(By.xpath(".//div[@class = 'row clerafix benefits']/div[position()=3]//span[@class = 'benefit-txt']"));
-        WebElement textUnderBaseIcon = driver.findElement(By.xpath(".//div[@class = 'row clerafix benefits']/div[position()=4]//span[@class = 'benefit-txt']"));
 
-        Assert.assertEquals(textUderPractiseIcon.getText(),
+
+        List<String> textContent = new ArrayList<String>();
+        List<WebElement> elements = driver.findElements(By.xpath(".//div[@class = 'row clerafix benefits']/div"));
+
+            for (WebElement element : elements){
+                String textValue = element.getText();
+                textContent.add(textValue);
+            }
+
+        assertEquals(textContent.get(0),
                 "To include good practices\n" +
                         "and ideas from successful\n" +
                         "EPAM project");
-        Assert.assertEquals(textUnderCustomItem.getText(),
+        assertEquals(textContent.get(1),
                 "To be flexible and\n" +
                         "customizable");
-        Assert.assertEquals(textUnderMultiIcon.getText(),
+        assertEquals(textContent.get(2),
                 "To be multiplatform");
-        Assert.assertEquals(textUnderBaseIcon.getText(),
+        assertEquals(textContent.get(3),
                 "Already have good base\n" +
                         "(about 20 internal and\n" +
                         "some external projects),\n" +
@@ -108,12 +116,12 @@ public class TestIndexPage {
 //        9. Assert a text of the main header
         WebElement mainText = driver.findElement(By.xpath(".//div[@class = 'main-content']//h3[@name = 'main-title']"));
 
-        Assert.assertEquals(mainText.getText(), "EPAM FRAMEWORK WISHES…");
+        assertEquals(mainText.getText(), "EPAM FRAMEWORK WISHES…");
 
 //        10. Assert a text of the sub header
         WebElement jdiText = driver.findElement(By.xpath(".//div[@class = 'main-content']//p[@name = 'jdi-text']"));
 
-        Assert.assertEquals(jdiText.getText(), "LOREM IPSUM DOLOR SIT AMET, " +
+        assertEquals(jdiText.getText(), "LOREM IPSUM DOLOR SIT AMET, " +
                 "CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA." +
                 " UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO" +
                 " CONSEQUAT DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.");
@@ -121,8 +129,8 @@ public class TestIndexPage {
 //         11. Assert that JDI GITHUB is a link and has a proper URL
         WebElement gitLinkToJDI = driver.findElement(By.xpath(".//div[@class = 'main-content']//h3[@class = 'text-center']/a"));
 
-        Assert.assertEquals(gitLinkToJDI.getText(), "JDI GITHUB");
-        Assert.assertEquals(gitLinkToJDI.getAttribute("href"), "https://github.com/epam/JDI");
+        assertEquals(gitLinkToJDI.getText(), "JDI GITHUB");
+        assertEquals(gitLinkToJDI.getAttribute("href"), "https://github.com/epam/JDI");
 
 //        12.Assert that there is Left Section
         WebElement leftSection = driver.findElement(By.xpath(".//body/div[@class ='wrapper']/div[@name = 'navigation-sidebar']"));
