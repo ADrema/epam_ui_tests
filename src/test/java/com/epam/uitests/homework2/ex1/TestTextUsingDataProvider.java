8cf38d8202029a064cd78f052bd9a61363534843
Anastasiia_Dremina <anastasyadremina@rambler.ru>
2018-05-24 20:26:35 +0300

    Homework 2. Ex.1 Style was formatted

--- src/test/java/com/epam/uitests/homework2/ex1/TestTextUsingDataProvider.java
+++ src/test/java/com/epam/uitests/homework2/ex1/TestTextUsingDataProvider.java
@@ -59,8 +59,6 @@ public class TestTextUsingDataProvider {
         };
     }
 
-
-
     @Test (dataProvider = "textElementsProvide")
     public void textValidationTest (String actualValue, String expectedValue){
 
