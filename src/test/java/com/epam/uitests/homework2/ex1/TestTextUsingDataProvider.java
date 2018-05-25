41fa202bc6d6aba7f36cf78542b1b4828eb7976f
Anastasiia_Dremina <anastasyadremina@rambler.ru>
2018-05-25 23:26:44 +0300

    Homework 1. Ex 1 - done

--- src/test/java/com/epam/uitests/homework2/ex1/TestTextUsingDataProvider.java
+++ src/test/java/com/epam/uitests/homework2/ex1/TestTextUsingDataProvider.java
@@ -1,83 +1,71 @@
 package com.epam.uitests.homework2.ex1;
-/*
- * Develop a dedicated test for asserting 4 texts below 4 pictures
- * on the Index Page -https://epam.github.io/JDI/index.html.
- * The test must be developed with help of the DataProvider.
- * Run it in the parallel by methods through the configuring parameters
- * in a @DataProvider annotation.
-*/
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.*;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 
-public class TestTextUsingDataProvider {
-    private WebDriver driver;
+public class TestTextUsingDataProvider{
 
-    @BeforeTest
-    public void beforeTest(){
-//        Set options to avoid browser pop-up windows
-        ChromeOptions options = new ChromeOptions();
+//    private WebDriver driver;
+    private ChromeOptions options;
+
+    private List<String> textContent = Arrays.asList(
+             "To include good practices\n" +
+             "and ideas from successful\n" +
+             "EPAM project",
+             "To be flexible and\n" +
+             "customizable",
+             "To be multiplatform",
+             "Already have good base\n" +
+             "(about 20 internal and\n" +
+             "some external projects),\n" +
+             "wish to get more…"
+    );
+
+    @BeforeClass
+    public void beforeClass(){
+        options = new ChromeOptions();
         options.addArguments("--disable-extensions");
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.navigate().to("https://epam.github.io/JDI/index.html");
     }
 
-    @AfterTest
-    public void afterTest(){
-        driver.close();
-        driver.quit();
-    }
-
-    @DataProvider
-    public Object[][] textElementsProvide() {
-
-        List<String> elementsText = getElementsText();
+    @DataProvider (parallel = true)
+    public Object[][] textElementsProvider() {
         return new Object[][] {
-                {elementsText.get(0), ("To include good practices\n" +
-                                                "and ideas from successful\n" +
-                                                "EPAM project")},
-                {elementsText.get(1), ("To be flexible and\n" +
-                                                "customizable")},
-                {elementsText.get(2), ("To be multiplatform")},
-                {elementsText.get(3), ("Already have good base\n" +
-                                                "(about 20 internal and\n" +
-                                                "some external projects),\n" +
-                                                "wish to get more…")}
+                {0, textContent.get(0)},
+                {1, textContent.get(1)},
+                {2, textContent.get(2)},
+                {3, textContent.get(3)}
         };
     }
 
-    @Test (dataProvider = "textElementsProvide")
-    public void textValidationTest (String actualValue, String expectedValue){
+    @Test (dataProvider = "textElementsProvider")
+    public void textValidationTest (int i, String expectedValue){
 
-//        2. Text content validations
-        assertEquals(actualValue,expectedValue);
-    }
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
 
-    public List<String> getElementsText(){
-        List<String> textContent = new ArrayList<String>();
+//      Open HomePage
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+//       Text content validations
         List<WebElement> elements = driver.findElements(By.xpath(".//div[@class = 'row clerafix benefits']/div"));
-
-        for (WebElement element : elements){
-            String textValue = element.getText();
-            textContent.add(textValue);
-        }
-        return textContent;
+        assertEquals(elements.get(i).getText(), expectedValue);
+        driver.quit();
 
     }
-
 }
 
 
