1d33d42920a26799ee6c0764416ed7f8a6d3f704
Anastasiia_Dremina <anastasyadremina@rambler.ru>
2018-06-18 17:37:20 +0300

    spaces were deleted

--- src/main/java/com/epam/ui/enumObjects/common/ServiceTabOptions.java
+++ src/main/java/com/epam/ui/enumObjects/common/ServiceTabOptions.java
@@ -5,19 +5,12 @@ import java.util.List;
 
 public enum ServiceTabOptions {
     SUPPORT("Support", "https://epam.github.io/JDI/support.html"),
-
     DATES("Dates", "https://epam.github.io/JDI/dates.html"),
-
     COMPLEXTABLE("Complex Table", "https://epam.github.io/JDI/complex-table.html"),
-
     SIMPLETBLE("Simple Table", "https://epam.github.io/JDI/simple-table.html"),
-
     USERTABLE("User Table", "https://epam.github.io/JDI/user-table.html"),
-
     TABLESANDPAGES("Table with pages", "https://epam.github.io/JDI/table-pages.html"),
-
     DIFFERENTELEMENTS("Different elements", "https://epam.github.io/JDI/different-elements.html"),
-
     PERFORMANCE("Performance", "https://epam.github.io/JDI/performance.html");
 
     public String name;
