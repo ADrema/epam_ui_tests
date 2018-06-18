9d1adb4bb9e2691f246e64a928a0d9d24c6c04fc
Anastasiia_Dremina <anastasyadremina@rambler.ru>
2018-06-18 17:35:09 +0300

    Homework 5. Was modified

--- src/test/java/com/epam/uitests/homework5/ex2/AllureSimpleSelenideTest1.java
+++ src/test/java/com/epam/uitests/homework5/ex2/AllureSimpleSelenideTest1.java
@@ -1,7 +1,6 @@
 package com.epam.uitests.homework5.ex2;
 
 import com.epam.ui.base.TestBaseForSelenide;
-import com.epam.ui.enumObjects.common.ServiceTabOptions;
 import com.epam.ui.enumObjects.differentElementsPage.CheckBoxesEnum;
 import com.epam.ui.enumObjects.differentElementsPage.RadioButtonEnum;
 import com.epam.ui.enumObjects.differentElementsPage.SelectEnum;
@@ -9,32 +8,38 @@ import com.epam.ui.enumObjects.homePage.Users;
 import com.epam.ui.pageObjects.selenide.DifferentElementsPage;
 import com.epam.ui.pageObjects.selenide.HomePage;
 import io.qameta.allure.Feature;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.page;
 
 @Feature("Test for Different Elements page")
 public class AllureSimpleSelenideTest1 extends TestBaseForSelenide {
     private HomePage homePage;
     private DifferentElementsPage dePage;
 
+    @BeforeClass
+    public void beforeClass() {
+        homePage = page(HomePage.class);
+        dePage = page(DifferentElementsPage.class);
+    }
+
     @Test()
     public void testIndexPageWithSelenide() {
 //        1. Open test site by URL
-        HomePage homePage = open("https://epam.github.io/JDI", HomePage.class);
+        homePage.open();
 
 //        2. Assert Browser title
-        homePage.checkPageTitleEqualsTo("Home Page");
+        homePage.checkHomePageTitle();
 
 //        3. Perform login
         homePage.signIn(Users.PITER_CHAILOVSKII);
 
-//        4. Assert User name in the left-top side of screen that user is loggined
-//        homePage.
+//        4. Assert User name in the left-top side of screen that user is loggined  homePage.
         homePage.checkUserName(Users.PITER_CHAILOVSKII);
-//
+
 //        5. Check interface on Home page, it contains all needed elements: 4 - pictures, 4 texts under them, 2 text above
-        homePage.checkBenefits(4);
+        homePage.checkBenefitssize();
         homePage.checkMainContentTexts();
 //
 //        6. Click on "Service" subcategory in the header and check that drop down contains options
@@ -44,12 +49,13 @@ public class AllureSimpleSelenideTest1 extends TestBaseForSelenide {
         homePage.checkSideBarServiceOptions();
 
 //        8. Open through the header menu Service -> Different Elements Page
-        dePage.open(ServiceTabOptions.DIFFERENTELEMENTS.url);
-//
+        dePage.open();
+
 //        9. Check interface on Different elements page, it contains all needed elements
-        dePage.checkNumberOfCheckBoxElements(4);
-        dePage.checkNumberOfRadioButtonElements(4);
-        dePage.checkNumberOfButtonsElements(2);
+        dePage.checkNumberOfCheckBoxElements();
+        dePage.checkNumberOfRadioButtonElements();
+        dePage.checkNumberOfButtonsElements();
+        dePage.checkDropDownMwnuIsPresent();
 
 //        10. Assert that there is Right Section
         dePage.checkFixPaneIsVisible();
@@ -58,21 +64,31 @@ public class AllureSimpleSelenideTest1 extends TestBaseForSelenide {
         dePage.checkNavBarIsVisible();
 
 //        12. Select checkboxes
-//        13. Assert that for each checkbox there is an individual log row and value is corresponded to the status of checkbox.
-        dePage.clickCheckboxElement(CheckBoxesEnum.WATER.index, true);
-        dePage.clickCheckboxElement(CheckBoxesEnum.WIND.index, true);
+        dePage.selectCheckboxElement(CheckBoxesEnum.WATER.index);
+        dePage.selectCheckboxElement(CheckBoxesEnum.WIND.index);
 
-//        14. Select radio
-//        15. Assert that for radiobutton there is a log row and value is corresponded to the status of radiobutton. 
+//        13. Assert that for each checkbox there is an individual log row and value is corresponded to the status of checkbox.
+        dePage.verifyCheckBoxLogRow(CheckBoxesEnum.WATER.index, true, 2);
+        dePage.verifyCheckBoxLogRow(CheckBoxesEnum.WIND.index, true, 1);
+
+//        13. Select radio
         dePage.selectRadioElement(RadioButtonEnum.SELEN.index);
 
-//        16. Select in dropdown
-//        17. Assert that for dropdown there is a log row and value is corresponded to the selected value.
+//        14. Assert that for radiobutton there is a log row and value is corresponded to the status of radiobutton. 
+        dePage.verifyRadioButtonLogRow(RadioButtonEnum.SELEN.index, 1);
+
+//        14. Select elements in dropdown
         dePage.selectDropDownElement(SelectEnum.YELLOW.index);
 
-//        18. Unselect and assert
-//        19. Assert that for each checkbox there is an individual log row and value is corresponded to the status of checkbox.
-        dePage.clickCheckboxElement(CheckBoxesEnum.WATER.index, false);
-        dePage.clickCheckboxElement(CheckBoxesEnum.WIND.index, false);
+//        15. Assert that for dropdown there is a log row and value is corresponded to the selected value.
+        dePage.verifyDropDownElementLogRow(SelectEnum.YELLOW.index, 1);
+
+//        15. Unselect checkboxes
+        dePage.selectCheckboxElement(CheckBoxesEnum.WATER.index);
+        dePage.selectCheckboxElement(CheckBoxesEnum.WIND.index);
+
+//        16. Assert that for each checkbox there is an individual log row and value is corresponded to the status of checkbox
+        dePage.verifyCheckBoxLogRow(CheckBoxesEnum.WATER.index, false, 2);
+        dePage.verifyCheckBoxLogRow(CheckBoxesEnum.WIND.index, false, 1);
     }
 }
--- src/test/java/com/epam/uitests/homework5/ex2/AllureSimpleSelenideTest2.java
+++ src/test/java/com/epam/uitests/homework5/ex2/AllureSimpleSelenideTest2.java
@@ -1,14 +1,14 @@
 package com.epam.uitests.homework5.ex2;
 
 import com.epam.ui.base.TestBaseForSelenide;
-import com.epam.ui.enumObjects.common.ServiceTabOptions;
 import com.epam.ui.enumObjects.homePage.Users;
 import com.epam.ui.pageObjects.selenide.DatesPage;
 import com.epam.ui.pageObjects.selenide.HomePage;
 import io.qameta.allure.Feature;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.page;
 
 @Feature("Test for Dates page")
 
@@ -16,38 +16,51 @@ public class AllureSimpleSelenideTest2 extends TestBaseForSelenide {
     private HomePage homePage;
     private DatesPage datesPage;
 
+    @BeforeClass
+    public void beforeClass() {
+        homePage = page(HomePage.class);
+        datesPage = page(DatesPage.class);
+    }
+
     @Test()
     public void testIndexPageWithSelenide() {
 //        1. Open test site by URL
-        HomePage homePage = open("https://epam.github.io/JDI", HomePage.class);
+        homePage.open();
 
 //        2. Assert Browser title
-        homePage.checkPageTitleEqualsTo("Home Page");
+        homePage.checkHomePageTitle();
 
 //        3. Perform login
         homePage.signIn(Users.PITER_CHAILOVSKII);
 
-//        4. Assert User name in the left-top side of screen that user is loggined
-//        homePage.
+//        4. Assert User name in the left-top side of screen that user is loggined homePage.
         homePage.checkUserName(Users.PITER_CHAILOVSKII);
 
 //        5. Open through the header menu Service -> Different Elements Page
-        datesPage.open(ServiceTabOptions.DATES.url);
+        datesPage.open();
 
 //        6. Using drag-and-drop set Range sliders. left sliders - the most left position, right slider - the most right position
-//        7. Using drag-and-drop set Range sliders. left sliders - the most left position, right slider - the most rigth position
         datesPage.moveSliders(0, 100);
 
-//        8. Using drag-and-drop set Range sliders. left sliders - the most left position, right slider - the most left position.
-//        9. Assert that for "From" and "To" sliders there are logs rows with corresponding values
+//        7. Assert log row
+        datesPage.verifyLogRow(0, 2, 100, 1);
+
+//        8. Using drag-and-drop set Range sliders. left sliders - the most left position, right slider - the most left position and assert the log row
         datesPage.moveSliders(0, 0);
 
-//        10. Using drag-and-drop set Range sliders. left sliders - the most rigth position, right slider - the most rigth position.
-//        11. Assert that for "From" and "To" sliders there are logs rows with corresponding values
+//        9. Assert log row
+        datesPage.verifyLogRow(0, 2, 0, 1);
+
+//        10. Using drag-and-drop set Range sliders. left sliders - the most rigth position, right slider - the most right position and assert the log row
         datesPage.moveSliders(100, 100);
 
-//        12. Using drag-and-drop set Range sliders.
-//        13. Assert that for "From" and "To" sliders there are logs rows with corresponding values
+//        11. Assert log row
+        datesPage.verifyLogRow(100, 1, 100, 2);
+
+//        12. Using drag-and-drop set Range sliders and assert the log row
         datesPage.moveSliders(30, 70);
+
+//        13. Assert log row
+        datesPage.verifyLogRow(30, 2, 70, 1);
     }
 }
